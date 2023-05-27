f9c8c7101adb58b57feaed2fddcae9ae06208be9
arteeis <darkness_ftv@mail.ru>
2023-05-27 13:19:32 +0800

    Task_005

---
+++ Task_005/Program.cs
@@ -0,0 +1,12 @@
+﻿ // Показать последнюю цифру трёхзначного числа.
+
+Console.Write("Здрасти! Введите пожалуйста трёхзначное число: ");
+int a = int.Parse(Console.ReadLine()!);
+int b = a % 10;
+
+if (a>99&&a<1000)
+{
+     string str = $"Последняя цифра: {b}";
+     Console.Write(str);
+}
+else Console.Write("Это не трех значное число, алло!");
